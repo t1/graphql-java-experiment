e62d2e8d74fd43987fe94b43cf7f7eec24dfe13e
Rüdiger zu Dohna <snackbox@sinntr.eu>
2020-03-23 17:38:53 +0100

    remove address book

--- src/main/java/com/example/addressbook/model/EmailAddress.java
+++
@@ -1,9 +0,0 @@
-package com.example.addressbook.model;
-
-import lombok.Value;
-
-public @Value class EmailAddress {
-    String value;
-
-    public static EmailAddress email(String value) { return new EmailAddress(value); }
-}
--- src/main/java/com/example/addressbook/model/PhoneNumber.java
+++
@@ -1,9 +0,0 @@
-package com.example.addressbook.model;
-
-import lombok.Value;
-
-public @Value class PhoneNumber {
-    String value;
-
-    public static PhoneNumber phoneNumber(String value) { return new PhoneNumber(value); }
-}
